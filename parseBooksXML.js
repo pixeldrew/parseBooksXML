5ffb40e786070dd39bdf15b08885d7f2df9676ed
drewf <drewf@cb.wireless.mssm.edu>
2012-01-08 00:32:34 -0500

    Updated package version and fixed xml parsing

--- parseBooksXML.js
+++ parseBooksXML.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs'),
   parseBooksXML = require('commander'),
-  expat = require('node-expat');
+  parser = require('xml2json');
 
 parseBooksXML
   .version('0.0.1')
@@ -12,11 +12,22 @@ parseBooksXML
 
 if(!parseBooksXML.file || !fs.statSync(parseBooksXML.file).isFile()) {
   console.error('File does not exist');
+  console.log(parseBooksXML.helpInformation());
   process.exit(1);
 }
 
-var booksXML = fs.readFileSync(parseBooksXML.file),
-p = new expat.Parser("utf-8"),
-books;
+var booksXML = fs.readFileSync(parseBooksXML.file), books, isbns = '';
 
-books = p.parse(booksXML);
+books = JSON.parse(parser.toJson(booksXML)).library.books;
+
+for(var i = 0;i < books.book.length;i++) {
+  var book = books.book[i];
+ 
+  if(book.identifier) {
+    isbns += book.identifier.value + ',';
+  } else {
+    console.error(book);
+  } 
+}
+
+console.log(isbns);
